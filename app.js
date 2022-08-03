1996771797cfea3423e273251d27558ca7de65e2
unknown <daveasmah@yahoo.com>
2022-08-02 22:42:13 -0400

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // Write a JavaScript function to calculate the factorial of a number.
 
+// Write a recursion function using if statement 
+function factorialNum(n) {
+    if (n < 0) {
+        return -1;  // reject any no#s less than 0
+    } else if (n === 0) {
+        return 1;  // no#s equal has a factorial of 1
+    }  else {
+        return (n * factorialNum(n - 1));  // calling the recursion function
+    }
+}
+
+console.log(factorialNum(6));  // prints 720
